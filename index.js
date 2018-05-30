03ac8ac4928fa4045b9eebaadd6e3651b8b67a09
KIMURA Tetsuro <haribote.nobody@gmail.com>
2018-05-30 15:33:07 +0900

    add optimization.

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const convert = require('./lib/convert')
+const optimize = require('./lib/optimize')
 
 exports.handler = async event => {
   const { base64Image, color, turdSize } = event
   const buffer = new Buffer(base64Image, 'base64')
   const svg = await convert(buffer, color, turdSize)
+  const optimised = await optimize(svg)
 
-  return new Buffer(svg).toString('base64')
+  return new Buffer(optimised).toString('base64')
 }
