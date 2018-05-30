09cc3b7441c97d92c12c98ddfb9b870b1ab9cf16
KIMURA Tetsuro <haribote.nobody@gmail.com>
2018-05-30 14:50:56 +0900

    add options.

--- __tests__/index.spec.js
+++ __tests__/index.spec.js
@@ -1,9 +1,26 @@
 const lambda = require('../index')
 const dummy = require('../__mocks__/dummy.jpg')
 
+function stringfy(base64) {
+  return new Buffer(base64, 'base64').toString()
+}
+
 describe('lambda-function-potrace', () => {
-  it('matches to snapshot', async () => {
-    const svg = await lambda.handler({ base64Image: dummy })
-    expect(new Buffer(svg, 'base64').toString()).toMatchSnapshot()
+  describe('default', () => {
+    it('matches to snapshot', async () => {
+      const svg = await lambda.handler({ base64Image: dummy })
+      expect(stringfy(svg)).toMatchSnapshot()
+    })
+  })
+
+  describe('set params', () => {
+    it('matches to snapshot', async () => {
+      const svg = await lambda.handler({
+        base64Image: dummy,
+        color: '#ccc',
+        turdSize: 10
+      })
+      expect(stringfy(svg)).toMatchSnapshot()
+    })
   })
 })
--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const convert = require('./lib/convert')
 
 exports.handler = async event => {
-  const { base64Image } = event
+  const { base64Image, color, turdSize } = event
   const buffer = new Buffer(base64Image, 'base64')
-  const svg = await convert(buffer)
+  const svg = await convert(buffer, color, turdSize)
 
   return new Buffer(svg).toString('base64')
 }
